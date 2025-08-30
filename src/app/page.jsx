d9811a0656b8079054aeaa8faff89da1053b9e91
rival fitrah <rivalfitrah9@gmail.com>
2025-08-30 20:10:47 +0700

    testing

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,10 @@
+import Hello from "@/components/Hello";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <p className="text-2xl font-bold">hello world</p>
+    <>
+      <Hello />
+    </>
   );
 }
